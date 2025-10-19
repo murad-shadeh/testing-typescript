00aeaa2410845a64fdb2ef405c32b06830370645
murad-dabbous <moradyousef04@gmail.com>
2025-10-19 08:19:10 +0000

    TypeScripts variables

--- basics.ts
+++ basics.ts
@@ -1,4 +1,4 @@
-const myPi: number = 3.14159;
+let myPi: number = 3.14159;
 console.log("HELLO");
 console.log("GOODBYE");
 Math.round(7.325678888);
---
+++ variables.ts
@@ -0,0 +1,2 @@
+let movieTitle: string = "Spider-Man: No Way Home";
+movieTitle = "Doctor Strange in the Multiverse of Madness";
