c5a55bab4b1dd3346cb93936b481c5a170509bc5
murad-dabbous <moradyousef04@gmail.com>
2025-10-26 06:04:37 +0000

    Function parameter annotation

---
+++ functions.ts
@@ -0,0 +1,9 @@
+function square(num: number) {
+  return num * num;
+}
+square(3);
+// square(true); this is wrong
+function greet(person: string) {
+  return `Hello, ${person}!`;
+}
+greet("Murad");
