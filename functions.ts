5787c18926dc12f04663f2cdf8ca12cb006a4dcb
murad-dabbous <moradyousef04@gmail.com>
2025-10-27 06:53:27 +0000

    More on function parameters annotation

--- functions.ts
+++ functions.ts
@@ -7,3 +7,9 @@ function greet(person: string) {
   return `Hello, ${person}!`;
 }
 greet("Murad");
+
+// trying arrow function
+const doSomething = (person: string, age: number, isFunny: boolean) => {
+  return `Hello, ${person}. You are ${age} years old, and are you Funny? ${isFunny}`;
+};
+doSomething("Murad", 25, true);
