d386b111d25c6ee0c9dd30dd736c0ccc2d15646f
murad-dabbous <moradyousef04@gmail.com>
2025-10-28 06:13:57 +0000

    function return types

--- functions.ts
+++ functions.ts
@@ -1,7 +1,7 @@
-function square(num: number) {
-  return num * num;
-}
-square(3);
+// function square(num: number) {
+//   return num * num;
+// }
+// square(3);
 // square(true); this is wrong
 // function greet(person: string) {
 //   return `Hello, ${person}!`;
@@ -16,7 +16,20 @@ doSomething("Murad", 25, true);
 
 // working with default parameters
 // we gave a default value to person parameter ... no need to pass any value for the function or we can if we need something else
-function greet(person: string = "stranger") {
+function greet(person: string = "stranger"): string {
   return `Hello, ${person}!`;
 }
 greet();
+// function return type
+function square(num: number): number {
+  return num * num;
+}
+square(3);
+
+// function that could return more than one type (union type)
+function random(num: number) {
+  if (Math.random() < 0.5) {
+    return num.toString();
+  }
+  return num;
+}
