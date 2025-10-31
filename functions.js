5c14f55e837ae773079d0bd3bc7e4974b9211785
murad-dabbous <moradyousef04@gmail.com>
2025-10-31 10:42:02 +0000

    Functions excercise

--- functions.js
+++ functions.js
@@ -7,13 +7,11 @@
 // //   return `Hello, ${person}!`;
 // // }
 // // greet("Murad");
-
 // // trying arrow function
 // const doSomething = (person: string, age: number, isFunny: boolean) => {
 //   return `Hello, ${person}. You are ${age} years old, and are you Funny? ${isFunny}`;
 // };
 // doSomething("Murad", 25, true);
-
 // // working with default parameters
 // // we gave a default value to person parameter ... no need to pass any value for the function or we can if we need something else
 // function greet(person: string = "stranger"): string {
@@ -25,7 +23,6 @@
 //   return num * num;
 // }
 // square(3);
-
 // // function that could return more than one type (union type)
 // function random(num: number) {
 //   if (Math.random() < 0.5) {
@@ -33,48 +30,42 @@
 //   }
 //   return num;
 // }
-
 // // Anonymous function contextual typing
 // const colors = ["red", "green", "blue"];
 // colors.map((color: string): string => {
 //   return color.toUpperCase();
 // });
-
 // // console.log(colors);
-
 // // A function that doesn't return anything (void)
 // const logMessage = (message: string): void => {
 //   console.log(message);
 // };
 // logMessage("Hello, World!");
-
 // // The never type of return
 // const throwError = (message: string): never => {
 //   throw new Error(message);
 // };
 // // throwError("This is an error!");
-
 // // or it never stops executing
 // const infiniteLoop = (): never => {
 //   while (true) {
 //     console.log("Looping forever...");
 //   }
 // };
-
 // lecture 26
 // Excercise 1
-const twofer = (name: string = "you"): string => {
-  return `One for ${name}, one for me.`;
+var twofer = function (name) {
+    if (name === void 0) { name = "you"; }
+    return "One for ".concat(name, ", one for me.");
 };
 console.log(twofer("Murad"));
 console.log(twofer());
-
 // Excercise 2
-const isLeapYear = (year: number): boolean => {
-  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-    return true;
-  }
-  return false;
+var isLeapYear = function (year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    }
+    return false;
 };
 console.log(isLeapYear(2012)); // true
 console.log(isLeapYear(2013)); // false
