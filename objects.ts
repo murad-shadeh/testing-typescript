1399428ba979fcc8ec7fb9f494fef9b84b7e2157
murad-dabbous <moradyousef04@gmail.com>
2025-11-03 08:13:24 +0000

    More object types

--- objects.ts
+++ objects.ts
@@ -3,3 +3,11 @@ function printName(name: { first: string; last: string }): void {
   console.log(`${name.first} ${name.last}`);
 }
 printName({ first: "Murad", last: "Pro" });
+
+// decalirng a avariable as an object in a specific pattern
+let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+
+// what the function return is an object with x and y number properties
+function randomCoordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
