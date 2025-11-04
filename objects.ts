9903d3488be4d0bddacc037116c0e0c0d71d4e30
murad-dabbous <moradyousef04@gmail.com>
2025-11-04 13:38:02 +0000

    Excess properties

--- objects.ts
+++ objects.ts
@@ -11,3 +11,8 @@ let coordinate: { x: number; y: number } = { x: 34, y: 2 };
 function randomCoordinate(): { x: number; y: number } {
   return { x: Math.random(), y: Math.random() };
 }
+
+printName({ first: "Mick", last: "Jagger", age: "24" }); // typeScript doesn't allow this because of excess property checking
+// but if we put this in a variable => TypeScript will allow it
+const singer = { first: "Mick", last: "Jagger", age: 24 };
+printName(singer);
