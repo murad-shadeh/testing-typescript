9b1f1c8a2197e9eb9a04f25bf61673596fd30332
murad-dabbous <moradyousef04@gmail.com>
2025-11-13 07:54:04 +0000

    Array types

---
+++ arrays.ts
@@ -0,0 +1,7 @@
+const activeUsers: string[] = [];
+activeUsers.push("Alice");
+// activeUsers.push(12);
+const ageList: number[] = [25, 30, 35];
+// change first element
+ageList[0] = 26;
+// ageList.push("Murad"); wrong only numbers allowed
