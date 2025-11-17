295eb4d502e80eb5295c14dbf2e61f3ae2aec31d
murad-dabbous <moradyousef04@gmail.com>
2025-11-17 16:22:48 +0000

    More on arrays types

--- arrays.ts
+++ arrays.ts
@@ -5,3 +5,12 @@ const ageList: number[] = [25, 30, 35];
 // change first element
 ageList[0] = 26;
 // ageList.push("Murad"); wrong only numbers allowed
+
+// Another way to declare arrays
+// const bools: Array<boolean> = [true, false, true];
+// this one is better
+const bools: boolean[] = [true, false, true];
+type Point = { x: number; y: number };
+const coords: Point[] = [];
+coords.push({ x: 2, y: 3 });
+// coords.push({ x: "2", y: 3 }); wrong because x should be number not string
