6d907a8111cb3fd247745caf784aff917c8e5556
murad-dabbous <moradyousef04@gmail.com>
2025-11-19 20:45:45 +0000

    multi dimensional arrays

--- arrays.ts
+++ arrays.ts
@@ -14,3 +14,12 @@ type Point = { x: number; y: number };
 const coords: Point[] = [];
 coords.push({ x: 2, y: 3 });
 // coords.push({ x: "2", y: 3 }); wrong because x should be number not string
+// multidimensional arrays
+// two dimensional array
+const board: string[][] = [
+  ["X", "O", "X"],
+  ["O", "X", "O"],
+  ["X", "O", "X"],
+];
+// three dimensional array
+const demo: number[][][] = [[[1]]];
