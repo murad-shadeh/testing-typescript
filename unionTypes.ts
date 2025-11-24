1760471333ede7fecc7f91010748be845fbbb238
murad-dabbous <moradyousef04@gmail.com>
2025-11-24 12:02:27 +0000

    Union types and narrowing

--- unionTypes.ts
+++ unionTypes.ts
@@ -12,3 +12,22 @@ type Loc = {
 };
 let coordinates: Point | Loc = { x: 10, y: 20 };
 coordinates = { lat: 40.7128, long: -74.006 };
+// Union types with functions
+function printAge(age: number | string): void {
+  console.log(`You're ${age} yours old.`);
+}
+printAge(25);
+printAge("twenty-five");
+
+function calculateTax(price: number | string, tax: number): number {
+  // this way we might multiply string with number which is not valid
+  // so to fix this we use type narrowing
+  if (typeof price === "string") {
+    price.replace("$", "");
+    //  parsefloat to convert string to number
+    return parseFloat(price) * tax;
+  } else {
+    //  if number => convert none and do the multiplication
+    return price * tax;
+  }
+}
