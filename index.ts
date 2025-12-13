800f9c11abc51c5c5292b809b2900ea5ae408957
murad-dabbous <moradyousef04@gmail.com>
2025-12-13 09:12:31 +0000

    Compiling to JavaScript

---
+++ index.ts
@@ -0,0 +1,11 @@
+// we need to play with the settings in the config file created by tsc --init
+interface Chicken {
+  breed: string;
+  eggsPerWeek: number;
+  name: string;
+}
+const norma: Chicken = {
+  breed: "Silkie",
+  eggsPerWeek: 4,
+  name: "norma",
+};
