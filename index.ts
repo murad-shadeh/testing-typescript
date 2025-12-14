04696ef1b3e5a66309d0744483d7c615a02a21bc
murad-dabbous <moradyousef04@gmail.com>
2025-12-14 09:52:14 +0000

    Watch mode

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ interface Chicken {
   breed: string;
   eggsPerWeek: number;
   name: string;
+  age: number;
 }
 const norma: Chicken = {
   breed: "Silkie",
   eggsPerWeek: 4,
   name: "norma",
+  age: 2,
 };
