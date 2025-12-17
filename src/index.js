10f5cb657bbe86238cd5085b46c4084d2599d10a
murad-dabbous <moradyousef04@gmail.com>
2025-12-17 19:25:53 +0000

    Include and Exclude options

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const norma = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "norma",
+    age: 2,
+};
+const juniper = {
+    breed: "Silkie",
+    eggsPerWeek: 7,
+    name: "norma",
+    age: 1,
+};
